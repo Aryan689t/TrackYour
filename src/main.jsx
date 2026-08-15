bc3d7f0cfc12f72a2e01bb5d8ad6dea4a6cec554
Aryan689t <tathagataryan2006@gmail.com>
2026-08-15 20:07:34 +0530

    Set up GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -14,33 +14,55 @@ import Roadmap from "./pages/dsa/Roadmap.jsx";
 import Journal from "./pages/dsa/Journal.jsx";
 
 
-import { createBrowserRouter,RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  {path:"/",
-    element :<App/>},
-  {path:"/academics",
-    element:<Academics/>},
-  {path:"/projects",
-    element:<Projects/>},
-    {path:"/network",
-    element:<Network/>},
-  {path:"/Semester/:id",
-    element:<Semester/>},
-   {path:"/Status",
-    element:<Status/>} ,
-    {path:"/History",
-    element:<History/>} ,
-    {path:"/Playlists",
-    element:<Playlists/>} ,
-    {path:"/Roadmap",
-    element:<Roadmap/>}, 
-    {path:"/Journal",
-    element:<Journal/>}, 
-]);
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/academics",
+    element: <Academics />
+  },
+  {
+    path: "/projects",
+    element: <Projects />
+  },
+  {
+    path: "/network",
+    element: <Network />
+  },
+  {
+    path: "/Semester/:id",
+    element: <Semester />
+  },
+  {
+    path: "/Status",
+    element: <Status />
+  },
+  {
+    path: "/History",
+    element: <History />
+  },
+  {
+    path: "/Playlists",
+    element: <Playlists />
+  },
+  {
+    path: "/Roadmap",
+    element: <Roadmap />
+  },
+  {
+    path: "/Journal",
+    element: <Journal />
+  },
+], {
+  basename: "/TrackYour/",
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
